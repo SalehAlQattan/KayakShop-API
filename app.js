b1abb9307e141c84df81f06504f3934fd321cf0e
Saleh AlQattan <salehalqattan@outlook.com>
2021-06-29 18:09:07 +0300

    add delete route

--- app.js
+++ app.js
@@ -4,14 +4,27 @@ const cors = require('cors');
 // new instance of express
 const app = express();
 // importing data
-const kayaks = require('./kayaks');
+let kayaks = require('./kayaks');
 
 // using cors to allow acces data // middleware
 app.use(cors());
 
+// =====================Start Routes========================== //
+// all kayaks
 app.get('/kayaks', (req, res) => {
   res.json(kayaks);
 });
-
+// deleting on kayak
+app.delete('/kayaks/:kayakId', (req, res) => {
+  const { kayakId } = req.params;
+  const foundKayak = kayaks.find((kayak) => kayak.id === +kayakId);
+  if (foundKayak) {
+    kayaks = kayaks.filter((kayak) => kayak.id != +kayakId);
+    res.status(204).end();
+  } else {
+    res.status(404).json({ message: `kayak Id ${kayakId} is not found` });
+  }
+});
+// =====================End Routes========================== //
 // running the server
 app.listen(8000, () => console.log('App is running on port 8000'));
