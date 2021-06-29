01b96307715d5f06a35f964c4cc2a72d105f5685
Saleh AlQattan <salehalqattan@outlook.com>
2021-06-29 16:35:47 +0300

    edit commit for the listening and cors

--- app.js
+++ app.js
@@ -6,11 +6,12 @@ const app = express();
 // importing data
 const kayaks = require('./kayaks');
 
-// using cors to allow accept data // middleWare
+// using cors to allow acces data // middleware
 app.use(cors());
 
 app.get('/kayaks', (req, res) => {
   res.json(kayaks);
 });
 
+// running the server
 app.listen(8000, () => console.log('App is running on port 8000'));
