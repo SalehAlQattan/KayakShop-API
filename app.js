c68005564101d24e96407f626b448c595e741d72
Saleh AlQattan <salehalqattan@outlook.com>
2021-06-30 16:37:49 +0300

    add post request functionality

--- app.js
+++ app.js
@@ -1,20 +1,25 @@
 // importing packages
 const express = require('express');
 const cors = require('cors');
+const slugify = require('slugify');
 // new instance of express
 const app = express();
 // importing data
 let kayaks = require('./kayaks');
 
-// using cors to allow acces data // middleware
+// ===================== start middleware ========================== //
+// using cors to allow acces data
 app.use(cors());
+// parsing body as json
+app.use(express.json());
+// ===================== end middleware ========================== //
 
 // =====================Start Routes========================== //
-// all kayaks
+// getting all kayaks route
 app.get('/kayaks', (req, res) => {
   res.json(kayaks);
 });
-// deleting on kayak
+// deleting kayak route
 app.delete('/kayaks/:kayakId', (req, res) => {
   const { kayakId } = req.params;
   const foundKayak = kayaks.find((kayak) => kayak.id === +kayakId);
@@ -24,6 +29,18 @@ app.delete('/kayaks/:kayakId', (req, res) => {
   } else {
     res.status(404).json({ message: `kayak Id ${kayakId} is not found` });
   }
+});
+// creating kayak route
+app.post('/kayaks', (req, res) => {
+  const id = kayaks.length + 1;
+  const slug = slugify(req.body.name, { lower: true });
+  const newKayak = {
+    id,
+    slug,
+    ...req.body,
+  };
+  kayaks.push(newKayak);
+  res.status(201).json(newKayak);
 });
 // =====================End Routes========================== //
 // running the server
