f525596de8b2805ca8eb1b16eea0a26dbe45ab77
Saleh AlQattan <salehalqattan@outlook.com>
2021-06-30 21:00:37 +0300

    refactor code by creating route file

---
+++ API/routes.js
@@ -0,0 +1,61 @@
+const express = require('express');
+// importing data
+let kayaks = require('../kayaks');
+// packages
+const slugify = require('slugify');
+const router = express.Router();
+
+// getting all kayaks route
+router.get('/', (req, res) => res.json(kayaks));
+// deleting kayak route
+router.delete('/:kayakId', (req, res) => {
+  // getting the kayak id from the url
+  const { kayakId } = req.params;
+  // check if the kayak is exists
+  const foundKayak = kayaks.find((kayak) => kayak.id === +kayakId);
+  // if the kayak exists send all the kayaks except this kayak
+  if (foundKayak) {
+    kayaks = kayaks.filter((kayak) => kayak.id != +kayakId);
+    res.status(204).end();
+  } else {
+    // if the kayak is not exists send a message says that it is not exists
+    res.status(404).json({ message: `kayak Id ${kayakId} is not found` });
+  }
+});
+// creating kayak route
+router.post('/', (req, res) => {
+  // setting new id manually
+  const id = kayaks.length + 1;
+  // creating slug for the new kayak
+  const slug = slugify(req.body.name, { lower: true });
+  // getting the new kayak from the front end
+  const newKayak = {
+    id,
+    slug,
+    ...req.body,
+  };
+  // adding the new kayak to the existing kayaks list
+  kayaks.push(newKayak);
+  // sending the new kayak to the front end
+  res.status(201).json(newKayak);
+});
+// update route
+router.put('/:kayakId', (req, res) => {
+  // getting the kayak id from the url
+  const { kayakId } = req.params;
+  // checking if the the kayak is exists
+  const foundKayak = kayaks.find((kayak) => kayak.id === +kayakId);
+  // if the kayak is exist get the data from front end
+  if (foundKayak) {
+    // get all the data icluding the field that is not updated by the client
+    for (const key in req.body) foundKayak[key] = req.body[key];
+    // setting the slug if the client didn't rename the kayak
+    foundKayak.slug = slugify(foundKayak.name, { lower: true });
+    res.status(204).end();
+  } else {
+    // if the kayak not found send a message said that the kayak is not found as JSON
+    res.status(404).json({ message: `kayak Id ${kayakId} is not found` });
+  }
+});
+
+module.exports = router;
--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 // importing packages
 const express = require('express');
 const cors = require('cors');
-const slugify = require('slugify');
 // new instance of express
 const app = express();
-// importing data
-let kayaks = require('./kayaks');
+// import routes
+const kayaksRoutes = require('./API/routes');
 
 // ===================== start middleware ========================== //
 // using cors to allow acces data
@@ -15,58 +14,7 @@ app.use(express.json());
 // ===================== end middleware ========================== //
 
 // =====================Start Routes========================== //
-// getting all kayaks route
-app.get('/kayaks', (req, res) => res.json(kayaks));
-// deleting kayak route
-app.delete('/kayaks/:kayakId', (req, res) => {
-  // getting the kayak id from the url
-  const { kayakId } = req.params;
-  // check if the kayak is exists
-  const foundKayak = kayaks.find((kayak) => kayak.id === +kayakId);
-  // if the kayak exists send all the kayaks except this kayak
-  if (foundKayak) {
-    kayaks = kayaks.filter((kayak) => kayak.id != +kayakId);
-    res.status(204).end();
-  } else {
-    // if the kayak is not exists send a message says that it is not exists
-    res.status(404).json({ message: `kayak Id ${kayakId} is not found` });
-  }
-});
-// creating kayak route
-app.post('/kayaks', (req, res) => {
-  // setting new id manually
-  const id = kayaks.length + 1;
-  // creating slug for the new kayak
-  const slug = slugify(req.body.name, { lower: true });
-  // getting the new kayak from the front end
-  const newKayak = {
-    id,
-    slug,
-    ...req.body,
-  };
-  // adding the new kayak to the existing kayaks list
-  kayaks.push(newKayak);
-  // sending the new kayak to the front end
-  res.status(201).json(newKayak);
-});
-// update route
-app.put('/kayaks/:kayakId', (req, res) => {
-  // getting the kayak id from the url
-  const { kayakId } = req.params;
-  // checking if the the kayak is exists
-  const foundKayak = kayaks.find((kayak) => kayak.id === +kayakId);
-  // if the kayak is exist get the data from front end
-  if (foundKayak) {
-    // get all the data icluding the field that is not updated by the client
-    for (const key in req.body) foundKayak[key] = req.body[key];
-    // setting the slug if the client didn't rename the kayak
-    foundKayak.slug = slugify(foundKayak.name, { lower: true });
-    res.status(204).end();
-  } else {
-    // if the kayak not found send a message said that the kayak is not found as JSON
-    res.status(404).json({ message: `kayak Id ${kayakId} is not found` });
-  }
-});
+app.use('/kayaks', kayaksRoutes);
 // =====================End Routes========================== //
 // running the server
 app.listen(8000, () => console.log('App is running on port 8000'));
